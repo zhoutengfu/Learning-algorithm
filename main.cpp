5a79fdb2c97d465f6e2ccd9431ab67a354d3873c
周藤福 <931945321@qq.com>
2018-09-10 23:56:41 +0800

    inserting sort

--- main.cpp
+++ main.cpp
@@ -16,10 +16,37 @@ void selectionSort(T arr[], int n) {
     }
 }
 
+//这个是我第一次写的
+//template<typename T>
+//void insertingSort(T arr[], int n) {
+//    for (int i = 1; i < n; ++i) {
+//        int tempIndex = i;
+//        for (int j = i - 1; j >= 0; j--) {
+//            if (arr[tempIndex] < arr[j]) {
+//                swap(arr[tempIndex], arr[j]);
+//                tempIndex = j;
+//            }
+//        }
+//    }
+//}
+
+template<typename T>
+void insertingSort(T arr[], int n) {
+    for (int i = 1; i < n; ++i) {
+        for (int j = i; j > 0; j--) {
+            if (arr[j] < arr[j - 1]) {
+                swap(arr[j], arr[j - 1]);
+            }
+        }
+    }
+}
+
+
 int main() {
-    int n = 10000;
+    int n = 100000;
     int *arr = SortTestHelper::generateRandomArray(n, 0, n);
     SortTestHelper::testSort("selection Sort", selectionSort, arr, n);
+    SortTestHelper::testSort("inserting Sort", insertingSort, arr, n);
 
     delete[] arr;
     return 0;
